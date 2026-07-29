cf407390ea60bf42b55d2b28218b36c973c2318a
Joseph Abdullaahi <josephabdullaahi@gmail.com>
2026-07-29 13:36:31 +0300

    feat(add-bookmark-button): responsive icon-only on mobile

--- src/components/add-bookmark-button.tsx
+++ src/components/add-bookmark-button.tsx
@@ -9,8 +9,13 @@ export function AddBookmarkButton() {
       className="text-preset-3! cursor-pointer rounded-lg text-primary-foreground!"
       size="lg"
     >
-      <Plus size="lg" />
-      Add Bookmark
+      <div className="block md:hidden">
+        <Plus size="lg" />
+      </div>
+      <div className="hidden items-center gap-1 md:flex">
+        <Plus size="lg" />
+        <span>Add Bookmark</span>
+      </div>
     </Button>
   )
 }
